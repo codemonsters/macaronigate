b6e4b9494e6a33f50447ae47bbaffbf24058ae48
Autotren <mail@uino.eu>
2023-10-21 15:22:27 +0200

    Changed renderer to mobile, fruits rain

--- src/minigames/frutas/main.gd
+++ src/minigames/frutas/main.gd
@@ -6,11 +6,24 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var fruta = fruta_scene.instantiate()
-	#fruta.position(100, 100)
-	add_child(fruta)
-
+	$FrutaTimer.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_fruta_timer_timeout():
+	var fruta = fruta_scene.instantiate()
+	var fruta_spawn_location = get_node("SpawnPath/SpawnLocation")
+	fruta_spawn_location.progress_ratio = randf()
+	
+	fruta.position = fruta_spawn_location.position
+	fruta.rotation = randf_range(-PI / 4, PI / 4)
+	fruta.angular_velocity = randf_range(0.5, 2)
+	
+	var velocity_x = (720 / 2 - fruta_spawn_location.position.x) * 0.7
+	var velocity_y = randf_range(-1000, -1700)
+	fruta.linear_velocity = Vector2(velocity_x, velocity_y)
+	
+	add_child(fruta) # Replace with function body.
