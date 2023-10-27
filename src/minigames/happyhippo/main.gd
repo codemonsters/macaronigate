163bc2e69888cbbcfa870eead7bc1200cc1bf078
Danielmg05 <92178121+Danielmg05@users.noreply.github.com>
2023-10-27 20:17:29 +0200

    Añadidas plataformas en happyhippo

---
+++ src/minigames/happyhippo/main.gd
@@ -0,0 +1,11 @@
+extends Node2D
+#Sonido hipopótamo: Mummy Zombie - Mike Koenig en soundbible.com/1059-Mummy-Zombie.html
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
