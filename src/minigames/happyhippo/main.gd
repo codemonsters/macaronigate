c3e9eb742a7bef69219f8189277def48f4e36cf8
Danielmg05 <92178121+Danielmg05@users.noreply.github.com>
2023-11-03 20:01:51 +0100

    Añadidas plataformas

--- src/minigames/happyhippo/main.gd
+++ src/minigames/happyhippo/main.gd
@@ -1,9 +1,23 @@
 extends Node2D
 #Sonido hipopótamo: Mummy Zombie - Mike Koenig en soundbible.com/1059-Mummy-Zombie.html
+var madera = preload("res://minigames/happyhippo/madera.tscn")
+var separacion = 100
+var num_plataformas
+var distancia
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	randomize()
+	num_plataformas = 720/(118+separacion)
+	num_plataformas = round(num_plataformas)
+	distancia = (720 - (118*num_plataformas+separacion*(num_plataformas-1)))/2
+	for y in range(4):
+		for x in range(num_plataformas+y%2):
+			var madera2 = madera.instantiate()
+			add_child(madera2)
+			madera2.position=Vector2((distancia)*((y+1)%2)+59+(2*59+separacion+(randi() % 41 - 20))*x,200+200*y)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
