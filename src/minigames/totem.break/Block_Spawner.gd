adc6d14ca245702656334cd7c0a98e14cbc11650
jorge-est <jorgeedecarrillo@gmail.com>
2023-11-10 20:03:03 +0100

    Actualizado Totem Break y juego de coches

--- src/minigames/totem.break/Block_Spawner.gd
+++ src/minigames/totem.break/Block_Spawner.gd
@@ -4,7 +4,6 @@ var block = preload("res://minigames/totem.break/Block.tscn")
 var array = []
 
 func _ready():
-	
 	for i in range(0, 20):
 		array.append(block.instantiate())
 		array [i].position.y = -205*i + 75
@@ -25,12 +24,16 @@ func _on_naranja_boton_button_down():
 
 
 func comprobarInput(colorBoton):
+	get_child(1).play()
 	if (!array.is_empty()):
 		if (array[0].get_child(0, 0).get_color() == colorBoton):
+			get_child(0).play()
 			array[0].queue_free()
 			array.remove_at(0)
 			bajarBloques()
 
 func bajarBloques():
+	var tween = get_tree().create_tween()
+	
 	for i in array:
-		i.position.y += 205
+		tween.tween_property(i, "position", Vector2(0, i.position.y + 205), 0.01)
