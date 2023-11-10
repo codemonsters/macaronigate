4892fb2f9931e4038ab456d58c8a8bc8759d55dd
asexto <a.sexto@gmail.com>
2023-11-10 20:02:33 +0100

    Cambiada animación del título
    Ahora usa cuerpos rígidos

--- src/menu.gd
+++ src/menu.gd
@@ -11,9 +11,5 @@ func _on_play_button_pressed():
 
 
 func _on_fade_in_animation_finished(anim_name):
-	$TitleSprite.get_node("AnimationPlayer").play("in")
-
-
-func _on_title_enter_animation_finished(anim_name):
-	var tween_title : Tween = get_tree().create_tween()
-	tween_title.tween_property($TitleSprite, "position:y", 450, 1).set_trans(Tween.TRANS_SPRING)
+	# TODO: Despertar al menú (debería estar sleep = true en un inicio)
+	pass
