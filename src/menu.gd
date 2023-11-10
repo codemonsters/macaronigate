7c765f5879acabf88be86ee30673497ae31d387c
asexto <a.sexto@gmail.com>
2023-11-10 19:49:00 +0100

    Añadido tween a la animación del título del menú

--- src/menu.gd
+++ src/menu.gd
@@ -3,10 +3,6 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#var tween_title : Tween = get_tree().create_tween()
-	
-	#tween_title.tween_property($TitleSprite, "position:y", 300, 0.4).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN)
-	#tween_title.tween_property($TitleSprite, "position:y", 250, 1).set_trans(Tween.TRANS_SINE)
 	$DissolveRect.get_node("AnimationPlayer").play("fade_in")
 
 
@@ -16,3 +12,8 @@ func _on_play_button_pressed():
 
 func _on_fade_in_animation_finished(anim_name):
 	$TitleSprite.get_node("AnimationPlayer").play("in")
+
+
+func _on_title_enter_animation_finished(anim_name):
+	var tween_title : Tween = get_tree().create_tween()
+	tween_title.tween_property($TitleSprite, "position:y", 450, 1).set_trans(Tween.TRANS_SPRING)
