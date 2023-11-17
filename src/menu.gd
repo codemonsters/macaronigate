a7e213ffa0bac35a3ca515aee047bf0fb891e1f9
asexto <a.sexto@gmail.com>
2023-11-17 19:29:33 +0100

    Actualizadas imágenes y animaciones del menú principal

--- src/menu.gd
+++ src/menu.gd
@@ -4,12 +4,9 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$DissolveRect.get_node("AnimationPlayer").play("fade_in")
+	$Title.get_node("AnimatedSprite2D").play("default")
+	$PlayButton.get_node("AnimatedSprite2D").play("default")
 
 
 func _on_play_button_pressed():
 	get_tree().change_scene("res://minigames/happyhippo/main.tscn")
-
-
-func _on_fade_in_animation_finished(anim_name):
-	# TODO: Despertar al menú (debería estar sleep = true en un inicio)
-	pass
