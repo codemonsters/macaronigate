6ee0cdb279680f372f1991c7ea16cf7ae984bda3
asexto <a.sexto@gmail.com>
2023-11-17 20:07:32 +0100

    Fuerza que la gravedad apunte hacia abajo al cargar el menú

--- src/menu.gd
+++ src/menu.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	PhysicsServer2D.area_set_param(get_world_2d().space, PhysicsServer2D.AREA_PARAM_GRAVITY_VECTOR, Vector2(0, 1))	# Establecemos la dirección de la gravedad hacia abajo (lo normal)
 	$DissolveRect.get_node("AnimationPlayer").play("fade_in")
 	$Title.get_node("AnimatedSprite2D").play("default")
 	$PlayButton.get_node("AnimatedSprite2D").play("default")
