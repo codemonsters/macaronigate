bc223e4e552ab1fc70b0cd62d454156afc5ee944
Autotren <mail@uino.eu>
2023-11-24 19:25:37 +0100

    Juego funcional

--- src/minigames/frutas/main.gd
+++ src/minigames/frutas/main.gd
@@ -5,15 +5,7 @@ signal game_over
 
 @export var fruta_scene: PackedScene
 var fruta_num = 0
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	$FrutaTimer.start()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+var game_seconds = 9
 
 func _on_fruta_timer_timeout():
 	var fruta = fruta_scene.instantiate()
@@ -30,38 +22,23 @@ func _on_fruta_timer_timeout():
 	
 	add_child(fruta)
 	fruta.add_to_group("needDelete")
-	# print("fruta")
-	
-#	fruta_num += 1
-#	if (fruta_num >= 20):
-#		$FrutaTimer.stop()
 
-#func _input(event):
-#	if event is InputEventMouseButton:
-#		if event.is_pressed():
-#			if event.position 
-#			print("Object clicked")
-#func _physics_process(delta):
-#	# get the Physics2DDirectSpaceState object
-#	var space = get_world_2d().direct_space_state
-#	# Get the mouse's position
-#	var mousePos = get_viewport().get_mouse_position()
-#	# Check if there is a collision at the mouse position
-#	var query = PhysicsPointQueryParameters2D.new()
-#	query.position = mousePos
-#
-#	if space.intersect_point(query, 1):
-#		print("hit something")
-#	else:
-#		print("no hit")
+func _on_killbox_body_entered(body):
+	if body.get_node("AnimatedSprite2D").get_frame() == 0:
+		game_over_func()
 
-# func _on_killbox_body_entered(body):
-# 	game_over.emit()
-# 	$FrutaTimer.stop()
-	
-# 	for obj in get_children():
-# 		if obj.is_in_group("needDelete"):
-# 			remove_child(obj)
-# 			obj.remove_from_group("needDelete")
-# 			obj.queue_free()
+func _on_game_timer_timeout():
+	game_seconds -= 1
+	$ColorRect/Label.set_text(str(game_seconds))
+	if game_seconds <= 0:
+		game_over_func()
 
+func game_over_func():
+	game_over.emit()
+	$FrutaTimer.stop()
+	$GameTimer.stop()
+	for obj in get_children():
+		if obj.is_in_group("needDelete"):
+			remove_child(obj)
+			obj.remove_from_group("needDelete")
+			obj.queue_free()
