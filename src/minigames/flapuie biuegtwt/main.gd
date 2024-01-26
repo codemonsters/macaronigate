1202ebe1450edb7ee484c045f8780137fc4c591c
asexto <a.sexto@gmail.com>
2024-01-26 20:01:10 +0100

    Genera tuberías a distintas alturas y añade pájaro

--- src/minigames/flapuie biuegtwt/main.gd
+++ src/minigames/flapuie biuegtwt/main.gd
@@ -1,17 +1,21 @@
 extends Node2D
 
+# Flying Bird Character (for the credits) thanks to bevouliin.com (https://opengameart.org/content/bevouliin-free-flying-bird-game-character-sprite-sheets)
+
 var pipe_scene = load("res://minigames/flapuie biuegtwt/pipe.tscn")
+var randomGenerator = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	_on_new_pipes_timer_timeout()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var pipes = $Pipes.get_children()
 	for pipe in pipes:
-		pipe.position.x -= 3
+		#pipe.position.x -= 200 * delta
+		pipe.position.x -= 200 * delta
 	
 
 
@@ -19,14 +23,14 @@ func _on_new_pipes_timer_timeout():
 	# highest lower pipe Y: 800, lowest lower pipe Y: 1680
 	# highest upper pipe Y: -400, lowest lower pipe Y: 480
 	
-	# TODO: expresanr la posición de una tubería respecto a la otra y añadir cierta aleatoriedad para que el alto del agujero sea variable
+	# TODO: Utilizar una función tipo Perlin Noise para no hacer cambios drásticos en las alturas de las tuberías
 	
 	var new_lower_pipe_instance = pipe_scene.instantiate()
-	new_lower_pipe_instance.set_position(Vector2(720 + 50, 1680))
+	new_lower_pipe_instance.set_position(Vector2(720 + new_lower_pipe_instance.get_width() / 2, randomGenerator.randi_range(800, 1680)))
 	$Pipes.add_child(new_lower_pipe_instance)
-	
+		
 	var new_upper_pipe_instance = pipe_scene.instantiate()
-	new_upper_pipe_instance.set_position(Vector2(720 + 50, 480))
+	new_upper_pipe_instance.set_position(Vector2(720 + new_upper_pipe_instance.get_width() / 2, new_lower_pipe_instance.get_position().y - 1200))
 	new_upper_pipe_instance.rotate(PI)
 	$Pipes.add_child(new_upper_pipe_instance)
 	
