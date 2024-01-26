cfb6f7aa63f43b2c353c93daef605b36307898f4
jorge-est <jorgeedecarrillo@gmail.com>
2024-01-26 19:58:57 +0100

    Añadida intro

---
+++ src/intro/AnimationPlayerController.gd
@@ -0,0 +1,10 @@
+extends AnimationPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	assigned_animation = "walking1"
+	current_animation = "walking1"
+
+
+
