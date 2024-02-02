f841c0cfa48edb4ca3fe0d3b54ae14f7be9fe84c
asexto <a.sexto@gmail.com>
2024-02-02 19:58:37 +0100

    Avances en el juevo ñarkanoid
    movimiento de la bola con velocidad constante, rebotes con paredes reimplementados, detección de colisión con el suelo añadida y añadido movimiento horizontal a la pala superior

--- src/minigames/ñarkanoid/main.gd
+++ src/minigames/ñarkanoid/main.gd
@@ -9,38 +9,31 @@ func _ready():
 	PhysicsServer2D.area_set_param(get_world_2d().space, PhysicsServer2D.AREA_PARAM_GRAVITY_VECTOR, Vector2(0, 0))	# Establecemos la dirección de la gravedad
 	$ball.set_axis_velocity(Vector2(0, 1000));
 
+
+func _physics_process(delta):
+	$ball.linear_velocity = $ball.linear_velocity.normalized() * 50000 * delta
+	
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	# movimiento horizontal del juegador
 	if paddle_moving_left and not paddle_moving_right:
-		$Paddle.position += Vector2(-300, 0)*delta
+		$Paddle.position += Vector2(-300, 0) * delta
 	elif not paddle_moving_left and paddle_moving_right:
-		$Paddle.position += Vector2(300, 0)*delta
-	else:
-		print("...")
-		print($ball.get_position()[0])
-	$ball.position = $ball.get_position()
+		$Paddle.position += Vector2(300, 0) * delta
 	
-	if $ball.get_position()[0] >= 720:
-		print("se escapa")
-		$ball.set_axis_velocity(Vector2(-1000, 0))
+	$Block.position.x = $ball.position.x	# la posición horizontal del bloque superior se corresponde siempre con el de la bola
 	
-	if $ball.get_position()[0] <= 0:
-		print("se escapa")
-		$ball.set_axis_velocity(Vector2(1000, 0))
+	#if $ball.get_position()[1] >= 1280:
+		#print("se escapa")
+		#$ball.set_axis_velocity(Vector2(0, -1000))
 	
-	if $ball.get_position()[1] >= 1280:
-		print("se escapa")
-		$ball.set_axis_velocity(Vector2(0, -1000))
-		
-	if $ball.get_position()[1] <= 0:
-		print("se escapa")
-		$ball.set_axis_velocity(Vector2(0, 1000))
 	
 func _input(event):
 	if event is InputEventScreenTouch:
 		#if event.is_pressed():
 		if event.pressed == true:
-			print("ha sido presseada en " + str(event.position[0]) + " y ancho de viewport = " +  str(get_viewport().get_visible_rect().size[0]))
+			#print("ha sido presseada en " + str(event.position[0]) + " y ancho de viewport = " +  str(get_viewport().get_visible_rect().size[0]))
 			if event.position[0] < get_viewport().get_visible_rect().size[0] / 2:
 				paddle_moving_left = true
 				paddle_moving_right = false
@@ -50,3 +43,8 @@ func _input(event):
 		else:
 			paddle_moving_left = false
 			paddle_moving_right = false
+
+
+func _on_floor_body_entered(body):
+	if body.get_name() == "ball":
+		print("Bola detectada!")
