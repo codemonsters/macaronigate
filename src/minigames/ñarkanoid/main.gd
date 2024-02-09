093ab90329e0a8f70955de88743f449f1482bbd3
asexto <a.sexto@gmail.com>
2024-02-09 19:07:51 +0100

    ñarkanoid: La pala ahora también se puede mover con el teclado

--- src/minigames/ñarkanoid/main.gd
+++ src/minigames/ñarkanoid/main.gd
@@ -24,16 +24,10 @@ func _process(delta):
 	
 	$Block.position.x = $ball.position.x	# la posición horizontal del bloque superior se corresponde siempre con el de la bola
 	
-	#if $ball.get_position()[1] >= 1280:
-		#print("se escapa")
-		#$ball.set_axis_velocity(Vector2(0, -1000))
-	
 	
 func _input(event):
 	if event is InputEventScreenTouch:
-		#if event.is_pressed():
 		if event.pressed == true:
-			#print("ha sido presseada en " + str(event.position[0]) + " y ancho de viewport = " +  str(get_viewport().get_visible_rect().size[0]))
 			if event.position[0] < get_viewport().get_visible_rect().size[0] / 2:
 				paddle_moving_left = true
 				paddle_moving_right = false
@@ -43,6 +37,14 @@ func _input(event):
 		else:
 			paddle_moving_left = false
 			paddle_moving_right = false
+	elif event.is_action_pressed("move_left"):
+		paddle_moving_left = true
+	elif event.is_action_released("move_left"):
+		paddle_moving_left = false
+	elif event.is_action_pressed("move_right"):
+		paddle_moving_right = true
+	elif event.is_action_released("move_right"):
+		paddle_moving_right = false
 
 
 func _on_floor_body_entered(body):
