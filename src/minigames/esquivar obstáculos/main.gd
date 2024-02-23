392c49c0de3dd0b6ca850297f3871b9102b6068f
DenizBL22 <147860956+DenizBL22@users.noreply.github.com>
2024-02-23 20:02:22 +0100

    Nuevo minijuego esquivar obstáculos

---
+++ src/minigames/esquivar obstáculos/main.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+signal game_over
+signal game_cleared
+
+@export var game_brief = "esquivar obstáculos"
+@export var needs_timer = true 
+@export var timer_seconds = 10
+
+func _ready():
+	pass 
+	
+func _process(delta):
+	pass
