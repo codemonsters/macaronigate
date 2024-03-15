d3115481cf995fd2d8cf6fcdd80274adbb641068
jorge-est <jorgeedecarrillo@gmail.com>
2024-03-15 20:01:12 +0100

    Cambiada mecánica de Ladrones de Diamantes

--- src/minigames/ladronesDeDiamantes/luzScript.gd
+++ src/minigames/ladronesDeDiamantes/luzScript.gd
@@ -4,7 +4,13 @@ var start = false
 var light_on = true
 var game_ended = false
 var flickering = false
-var flicker_time = 0.1
+var progress = 100
+var stealing = false
+
+var max_dark_time = 2
+
+const progress_factor = 170
+const losing_factor = 700
 
 signal game_win
 signal game_lose
@@ -15,35 +21,52 @@ func _ready():
 
 func _on_timer_timeout():
 	if (!start):
-		$TimerStartEnd.wait_time = RandomNumberGenerator.new().randf_range(2.5, 5)
 		start = true
-		$TimerStartEnd.start()
-	else:
-		light_on = false
-		light_energy = 0
-
-
-func _on_area_3d_input_event(_camera, event, _position, _normal, _shape_idx):
-	if !game_ended and start:
-		if event is InputEventMouseButton:
-			if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-				if light_energy == 0:
-					game_win.emit()
-				else:
-					game_ended = true
-					$TimerStartEnd.stop()
-					game_lose.emit()
 
 
 func _on_timer_flickers_timeout():
 	if light_on:
 		if flickering:
-			$TimerFlickers.wait_time = RandomNumberGenerator.new().randf_range(0.2, 0.4)
+			$TimerFlickers.wait_time = RandomNumberGenerator.new().randf_range(0.5, 2) # Tiempo APAGADO
 			light_energy = 0
 		else:
-			$TimerFlickers.wait_time = RandomNumberGenerator.new().randf_range(0.1, flicker_time)
+			$TimerFlickers.wait_time = RandomNumberGenerator.new().randf_range(1, 2.5) # Tiempo ENCENDIDO
 			light_energy = 16
 		flickering = !flickering
-		flicker_time += 0.01
+		max_dark_time += 0.2
 	else:
 		$TimerFlickers.stop()
+
+
+func _process(delta):
+	if stealing:
+		if light_energy == 0:
+			progress += progress_factor * delta
+		else:
+			progress -= losing_factor * delta 
+	if !game_ended:
+		if progress >= 400:
+			game_ended = true
+			game_win.emit()
+			stealing = false
+		elif progress <= 0:
+			game_ended = true
+			game_lose.emit()
+			stealing = false
+	get_parent().get_child(1).size.x = progress # NO MOVER PROGRESSBAR
+	
+
+
+func _on_steal_button_down():
+	if !game_ended:
+		stealing = true
+
+
+func _on_steal_button_up():
+	stealing = false
+
+
+func _on_main_game_over():
+	if !game_ended:
+		game_lose.emit()
+	game_ended = true
