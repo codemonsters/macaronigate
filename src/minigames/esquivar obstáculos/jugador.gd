25106698bdd08e036da7b78b4e8ba6ef79a0530f
Danielmg05 <92178121+Danielmg05@users.noreply.github.com>
2024-03-22 19:57:54 +0100

    Añadido salto a esquivar obstáculos

--- src/minigames/esquivar obstáculos/jugador.gd
+++ src/minigames/esquivar obstáculos/jugador.gd
@@ -1,28 +1,19 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var t = 0
+var vx = 70
+var vy = -310
+var a = 275
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	pass
+	
+func jump(delta):
+	t += delta
+	velocity.x = vx
+	velocity.y = vy + a*t
 	move_and_slide()
