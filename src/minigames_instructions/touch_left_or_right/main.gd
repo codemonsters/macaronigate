3951a67aeed5854e145a29c2f0333343e62d0089
alo-seo <alofalas@gmail.com>
2024-04-05 20:10:24 +0200

    refactorizado las introducciones

---
+++ src/minigames_instructions/touch_left_or_right/main.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+const wave_delay = 0.2
+var scene = preload("res://minigames_instructions/Wave.tscn")
+var wave_count = 0
+
+
+func _ready():
+	pass
+
+
+func _process(delta):
+	pass
+
+func touch(pos: int, count: int = 1): # Nombre del marcador y cuántos círculos añadir
+	var lista_nodos = get_node("Posiciones puntos")
+	for i in range(1, count+1):
+		var scene_instance: Sprite2D = scene.instantiate()
+		$Waves.add_child(scene_instance)
+		var nodo_posicion = lista_nodos.get_node_or_null("WavePos"+ str(pos))
+		assert(nodo_posicion != null, "¡Esa posición no existe! Créala :(")
+		scene_instance.position = nodo_posicion.position
+		
+		print(wave_delay * i)
+		scene_instance.get_node("Timer").wait_time = wave_delay * i
+		scene_instance.get_node("Timer").start()
+	
