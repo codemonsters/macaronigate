996cc5664e14b72fe2fc5102523b1dddeb6cce38
xJuanchoux <jundanfernandezp@gmail.com>
2024-05-10 19:31:05 +0200

    inteto del juego de bandeja

---
+++ src/minigames/Bandejita/bandeja.gd
@@ -0,0 +1,34 @@
+extends Node2D
+var left = 0
+var right = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right") or right == 1:
+			velocity.x += 1.25
+	if Input.is_action_pressed("move_left") or left == 1:
+			velocity.x -= 1.25
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * 400
+		
+	position += velocity * delta
+	position.x = clamp(position.x, 138.77, 570.41)
+
+func _on_left_button_up():
+	left = 0
+
+func _on_left_button_down():
+	left = 1
+
+
+func _on_right_button_down():
+	right = 1
+
+
+func _on_right_button_up():
+	right = 0
