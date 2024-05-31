3894723321c8d2918bc45304bd3cbf66275cdd94
Danielmg05 <92178121+Danielmg05@users.noreply.github.com>
2024-05-31 19:55:04 +0200

    Retocado cuchina disastrosa

--- src/minigames/cuchina disastrosa/jugador.gd
+++ src/minigames/cuchina disastrosa/jugador.gd
@@ -1,20 +1,23 @@
 extends CharacterBody2D
 
+var fall = false
 var t = 0
-var vx = 70
-var vx_side = 200
-var vy_up = -310
-var vy_down = -125
+var vx = 120
+var vx_side = 300
+var vy_up = -475
+var vy_down = -150
 var vy_side = -75
-var a = 275
-var a_side = 150
+var a_up = 675
+var a_down = 700
+var a_side = 200
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
 func _physics_process(delta):
-	pass
+	if fall == true:
+		velocity.y += 100*delta
+		move_and_slide()
 	
 func jump(delta, direction):
 	if direction.y == 1:
@@ -27,13 +30,13 @@ func jump(delta, direction):
 func jump_up(delta, direction):
 	t += delta
 	velocity.x = vx*direction.x
-	velocity.y = vy_up + a*t
+	velocity.y = vy_up + a_up*t
 	move_and_slide()
 
 func jump_down(delta, direction):
 	t += delta
 	velocity.x = vx*direction.x
-	velocity.y = vy_down + a*t
+	velocity.y = vy_down + a_down*t
 	move_and_slide()
 	
 func jump_side(delta, direction):
@@ -41,3 +44,8 @@ func jump_side(delta, direction):
 	velocity.x = vx_side*direction.x
 	velocity.y = vy_side + a_side*t
 	move_and_slide()
+
+func die(delta):
+	$CollisionShape2D.free()
+	rotate(-1)
+	fall = true
