b503aaafcb6beed62c9383cce25c10e259a0b49c
Autotren <mail@uino.eu>
2024-06-14 20:07:28 +0200

    cambiada musica de death_screen

--- src/death_screen/MusicPlayer.gd
+++ src/death_screen/MusicPlayer.gd
@@ -1,9 +1,10 @@
 extends AudioStreamPlayer
 
 var loopSet = false
+var songLooped = preload("res://death_screen/deathMusic/deathmusicloop.wav")
 
 func _on_finished():
 	if !loopSet:
-		self.set_stream(preload("res://death_screen/deathMusic/deathmusicloop.wav"))
+		self.set_stream(songLooped)
 		loopSet = true
 	self.play()
