172cf547bd7df01a30b44d7c5313ce773fdfb0df
jorge-est <jorgeedecarrillo@gmail.com>
2024-06-28 20:10:07 +0200

    Mejorada animación de victoria

--- src/win_screen/CameraMovement.gd
+++ src/win_screen/CameraMovement.gd
@@ -3,10 +3,17 @@ extends Camera3D
 const ROTATION_SPEED = 1
 const AMPLITUDE = 3
 
+var spinning = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta):
-	rotation += Vector3(0, ROTATION_SPEED, 0) * delta
-	
-	position.x = AMPLITUDE * sin(rotation.y)
-	position.z = AMPLITUDE * cos(rotation.y)
+	if spinning:
+		rotation += Vector3(0, ROTATION_SPEED, 0) * delta
+		
+		position.x = AMPLITUDE * sin(rotation.y)
+		position.z = AMPLITUDE * cos(rotation.y)
+
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "walk_forward":
+		spinning = true
